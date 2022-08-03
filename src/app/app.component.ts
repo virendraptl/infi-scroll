de0be97cc09b6d67d3d85ac38ee6a7f909c60c34
virendraptl <virendra.patil253@gmail.com>
2022-08-03 18:46:12 +0530

    scroll test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'infi-scroll';
+  array = [];
+  sum = 10;
+  throttle = 300;
+  scrollDistance = 1;
+  scrollUpDistance = 2;
+  direction = '';
+
+  constructor() {
+    this.appendItems(0, this.sum);
+  }
+
+  onScrollDown(ev) {
+    console.log('scrolled down!!', ev);
+
+    const start = this.sum;
+    this.sum += 20;
+    this.appendItems(start, this.sum);
+
+    this.direction = 'down';
+  }
+
+  addItems(startIndex, endIndex, _method) {
+    for (let i = 0; i < this.sum; ++i) {
+      this.array[_method]([i, ' ', this.generateWord()].join(''));
+    }
+  }
+
+  appendItems(startIndex, endIndex) {
+    this.addItems(startIndex, endIndex, 'push');
+  }
+
+  generateWord() {
+    return 'asffghjk';
+  }
 }
